7142cfff42bd8c502f41deae4df6f5dad7b9c4df
EntrenaDev <manuel.entrena@gmail.com>
2023-11-30 20:41:09 +0100

    preparamos dashboard para traer datos

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { UserCard } from "./sections/users/UserCard";
-import { useUsers } from "./sections/users/useUsers";
+import { Dashboard } from "./components";
 
 export function App() {
-	const users = useUsers();
-
 	return (
 		<div className="App">
-			<h3>⚡⚛️ React Best Practices </h3>
-			<h2>Current users</h2>
-
-			{users.map((user) => (
-				<UserCard key={user.name} user={user} />
-			))}
+			<h3>⚡⚛️ React Best Practices</h3>
+			<Dashboard />
 		</div>
 	);
 }
