521495f52952fda418d3008e42e54d660a135bdf
EntrenaDev <manuel.entrena@gmail.com>
2023-12-04 22:24:37 +0100

    Added components repository with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { Dashboard } from "./components";
 
 export function App() {
-	return (
-		<div className="App">
-			<h3>⚡⚛️ React Best Practices</h3>
-			<Dashboard />
-		</div>
-	);
+	return <Dashboard />;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
-import React from "react";
+import "./assets/styles/index.scss";
+
 import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+root.render(<App />);
