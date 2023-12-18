5e164f6335b3def02e45384ecbe78a7e81e08350
EntrenaDev <manuel.entrena@gmail.com>
2023-12-18 17:34:00 +0100

    Added testing

--- tests/Dashboard.spec.tsx
+++ tests/Dashboard.spec.tsx
@@ -21,6 +21,43 @@ describe("DashBoard section", () => {
 			name: new RegExp("DevDash", "i"),
 		});
 
+		const firstWidgetTitle = `${githubApiResponses[0].repositoryData.owner.login}/${githubApiResponses[0].repositoryData.name}`;
+		const firstWidgetHeader = await screen.findByRole("link", {
+			name: new RegExp(firstWidgetTitle, "i"),
+		});
+
 		expect(title).toBeInTheDocument();
+		expect(firstWidgetHeader).toBeInTheDocument();
+	});
+
+	it("show not results message when there are no widgets", async () => {
+		mockRepository.mockImplementationOnce(() => {
+			return {
+				search: () => Promise.resolve([]),
+			} as unknown as GitHubApiGitHubRepositoryRepository;
+		});
+
+		render(<Dashboard />);
+
+		const noResults = await screen.findByText(new RegExp("No hay widgets configurados", "i"));
+
+		expect(noResults).toBeInTheDocument();
+	});
+
+	it("show last modified date in human readable format", async () => {
+		const mockedResponse = [...githubApiResponses];
+		mockedResponse[0].repositoryData.updated_at = new Date().toISOString();
+
+		mockRepository.mockImplementationOnce(() => {
+			return {
+				search: () => Promise.resolve(mockedResponse),
+			} as unknown as GitHubApiGitHubRepositoryRepository;
+		});
+
+		render(<Dashboard />);
+
+		const modificationDate = await screen.findByText(new RegExp("today", "i"));
+
+		expect(modificationDate).toBeInTheDocument();
 	});
 });
