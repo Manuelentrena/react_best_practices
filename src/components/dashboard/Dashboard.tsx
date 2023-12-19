dcc19139cf296492acf1e79728d784b7b13393af
EntrenaDev <manuel.entrena@gmail.com>
2023-12-19 15:40:03 +0100

    Added widget compoenet dashboard

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -4,32 +4,8 @@ import { config } from "../../devdash_config";
 import { GitHubRepository } from "../../domain/GitHubRepository";
 import { GitHubRepositoryRepository } from "../../domain/GitHubRepositoryRepository";
 import Brand from "./brand.svg";
-import Check from "./check.svg";
 import styles from "./Dashboard.module.scss";
-import Error from "./error.svg";
-import PullRequests from "./git-pull-request.svg";
-import IssueOpened from "./issue-opened.svg";
-import Lock from "./lock.svg";
-import Forks from "./repo-forked.svg";
-import Start from "./star.svg";
-import Unlock from "./unlock.svg";
-import Watchers from "./watchers.svg";
-
-const isoToReadableDate = (lastUpdateDate: Date): string => {
-	const currentDate = new Date();
-	const diffTime = currentDate.getTime() - lastUpdateDate.getTime();
-	const diffDays = Math.round(diffTime / (1000 * 3600 * 24));
-
-	if (diffDays === 0) {
-		return "today";
-	}
-
-	if (diffDays > 30) {
-		return "more than a month ago";
-	}
-
-	return `${diffDays} days ago`;
-};
+import { GitHubRepositoryWidget } from "./GitHubRepositoryWidget";
 
 const Dashboard = ({ repository }: { repository: GitHubRepositoryRepository }) => {
 	const title = "DevDash";
@@ -58,53 +34,11 @@ const Dashboard = ({ repository }: { repository: GitHubRepositoryRepository }) =
 				</div>
 			) : (
 				<section className={styles.container}>
-					{repositoryData.map((widget) => (
-						<article className={styles.widget} key={widget.id.name}>
-							<header className={styles.widget__header}>
-								<h2 className={styles.widget__title}>
-									<a
-										href={widget.url}
-										target="_blank"
-										title={`${widget.id.organization}/${widget.id.name}`}
-										rel="noreferrer"
-									>
-										{widget.id.organization}/{widget.id.name}
-									</a>
-								</h2>
-								{widget.private ? <Lock /> : <Unlock />}
-							</header>
-							<div className={styles.widget__body}>
-								<div className={styles.widget__status}>
-									<p>Last update {isoToReadableDate(widget.updatedAt)}</p>
-									{widget.hasWorkflows && (
-										<div>{widget.isLastWorkflowSuccess ? <Check /> : <Error />}</div>
-									)}
-								</div>
-								<p className={styles.widget__description}>{widget.description}</p>
-							</div>
-							<footer className={styles.widget__footer}>
-								<div className={styles.widget__stat}>
-									<Start />
-									<span>{widget.stars}</span>
-								</div>
-								<div className={styles.widget__stat}>
-									<Watchers />
-									<span>{widget.watchers}</span>
-								</div>
-								<div className={styles.widget__stat}>
-									<Forks />
-									<span>{widget.forks}</span>
-								</div>
-								<div className={styles.widget__stat}>
-									<IssueOpened />
-									<span>{widget.issues}</span>
-								</div>
-								<div className={styles.widget__stat}>
-									<PullRequests />
-									<span>{widget.pullRequests}</span>
-								</div>
-							</footer>
-						</article>
+					{repositoryData.map((repository) => (
+						<GitHubRepositoryWidget
+							key={`${repository.id.organization}/${repository.id.name}`}
+							repository={repository}
+						/>
 					))}
 				</section>
 			)}
