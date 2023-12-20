7609a869823799675c535c7b6c70baab61339a76
EntrenaDev <manuel.entrena@gmail.com>
2023-12-20 15:05:18 +0100

    Changed tests which included router

--- tests/Dashboard.spec.tsx
+++ tests/Dashboard.spec.tsx
@@ -1,37 +1,43 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { mock } from "jest-mock-extended";
 
-import { Dashboard } from "../src/components";
+import { Dashboard, Layout } from "../src/components";
 import { GitHubRepositoryRepository } from "../src/domain/GitHubRepositoryRepository";
 import { GitHubRepositoryMother } from "./GitHubRepositoryMother";
+import { renderWithRouter } from "./renderWithRouter";
 
 const mockRepository = mock<GitHubRepositoryRepository>();
 
 describe("DashBoard section", () => {
+	it("show title in layout", async () => {
+		renderWithRouter(<Layout />);
+
+		const title = await screen.findByRole("heading", {
+			name: new RegExp("DevDash", "i"),
+		});
+
+		expect(title).toBeInTheDocument();
+	});
+
 	it("show all widgtes", async () => {
 		const gitHubRepository = GitHubRepositoryMother.create();
 
 		mockRepository.search.mockResolvedValue([gitHubRepository]);
 
-		render(<Dashboard repository={mockRepository} />);
-
-		const title = await screen.findByRole("heading", {
-			name: new RegExp("DevDash", "i"),
-		});
+		renderWithRouter(<Dashboard repository={mockRepository} />);
 
 		const firstWidgetTitle = `${gitHubRepository.id.organization}/${gitHubRepository.id.name}`;
 		const firstWidgetHeader = await screen.findByRole("link", {
 			name: new RegExp(firstWidgetTitle, "i"),
 		});
 
-		expect(title).toBeInTheDocument();
 		expect(firstWidgetHeader).toBeInTheDocument();
 	});
 
 	it("show not results message when there are no widgets", async () => {
 		mockRepository.search.mockResolvedValue([]);
 
-		render(<Dashboard repository={mockRepository} />);
+		renderWithRouter(<Dashboard repository={mockRepository} />);
 
 		const noResults = await screen.findByText(new RegExp("No hay widgets configurados", "i"));
 
@@ -43,7 +49,7 @@ describe("DashBoard section", () => {
 
 		mockRepository.search.mockResolvedValue([gitHubRepository]);
 
-		render(<Dashboard repository={mockRepository} />);
+		renderWithRouter(<Dashboard repository={mockRepository} />);
 
 		const modificationDate = await screen.findByText(new RegExp("today", "i"));
 
