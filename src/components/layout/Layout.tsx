e1eff9b3379506e1e29d79e613025949b4cd64f6
EntrenaDev <manuel.entrena@gmail.com>
2023-12-21 19:08:54 +0100

    Added TopbarProgress component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import TopBarProgress from "react-topbar-progress-indicator";
 
-import { ErrorBoundary } from "../ErrorBoundary";
+import { ErrorBoundary, TopBarProgressByLocation } from "../../components";
 import Brand from "./brand.svg";
 import styles from "./Layout.module.scss";
 
 const title = "DevDash";
+TopBarProgress.config({
+	barColors: {
+		"0": "#fff",
+		"1.0": "#3cff64",
+	},
+	shadowBlur: 5,
+});
 
 export function Layout() {
 	return (
 		<>
+			<TopBarProgressByLocation />
 			<header className={styles.header}>
 				<section className={styles.header__container}>
-					<Brand />
-					<h1 className={styles.app__brand}>{title}</h1>
+					<a href="https://codely.com">
+						<Brand />
+					</a>
+					<Link to={`/`}>
+						<h1 className={styles.app__brand}>{title}</h1>
+					</Link>
 				</section>
 			</header>
 			<ErrorBoundary>
