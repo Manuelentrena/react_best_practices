77880256751fe32347059db1e2c98a4f9a8664d9
EntrenaDev <manuel.entrena@gmail.com>
2023-12-21 19:10:06 +0100

    Added pullrequest infraestructure

--- src/infrastructure/GitHubApiGitHubRepositoryRepository.ts
+++ src/infrastructure/GitHubApiGitHubRepositoryRepository.ts
@@ -1,6 +1,6 @@
 import { GitHubRepository } from "../domain/GitHubRepository";
 import { GitHubRepositoryRepository } from "../domain/GitHubRepositoryRepository";
-import { CiStatus, PullRequest, RepositoryData } from "./GitHubApiResponse";
+import { CiStatus, RepositoryData } from "./GitHubApiResponse";
 
 interface RepositoryId {
 	organization: string;
@@ -10,7 +10,6 @@ interface RepositoryId {
 export class GitHubApiGitHubRepositoryRepository implements GitHubRepositoryRepository {
 	private readonly endpoints = [
 		"https://api.github.com/repos/$organization/$name",
-		"https://api.github.com/repos/$organization/$name/pulls",
 		"https://api.github.com/repos/$organization/$name/actions/runs?page=1&per_page=1",
 	];
 
@@ -41,11 +40,7 @@ export class GitHubApiGitHubRepositoryRepository implements GitHubRepositoryRepo
 		return Promise.all(repositoryRequests)
 			.then((responses) => Promise.all(responses.map((response) => response.json())))
 			.then((responses) => {
-				const [repositoryData, pullRequests, ciStatus] = responses as [
-					RepositoryData,
-					PullRequest[],
-					CiStatus
-				];
+				const [repositoryData, ciStatus] = responses as [RepositoryData, CiStatus];
 
 				return {
 					id: {
@@ -65,7 +60,6 @@ export class GitHubApiGitHubRepositoryRepository implements GitHubRepositoryRepo
 					watchers: repositoryData.watchers_count,
 					forks: repositoryData.forks_count,
 					issues: repositoryData.open_issues_count,
-					pullRequests: pullRequests.length,
 					workflowRunsStatus: ciStatus.workflow_runs.map((run) => ({
 						id: run.id,
 						name: run.name,
