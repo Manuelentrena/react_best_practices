9e59ea40f8491cf2706578a85372fa715d9206b0
EntrenaDev <manuel.entrena@gmail.com>
2023-12-31 14:41:46 +0100

    Added new hook add repository widget

--- src/pages/dashBoard/DashboardFactory.tsx
+++ src/pages/dashBoard/DashboardFactory.tsx
@@ -3,11 +3,15 @@ import React from "react";
 import { Dashboard } from "../../components";
 import { config } from "../../devdash_config";
 import { GitHubApiGitHubRepositoryRepository } from "../../infrastructure/GitHubApiGitHubRepositoryRepository";
+import { LocalStorageRepositoryWidgetRepository } from "../../infrastructure/LocalStorageWidgetRepository";
 
 const repository = new GitHubApiGitHubRepositoryRepository(config.github_access_token);
+const repositoryWidgetRepository = new LocalStorageRepositoryWidgetRepository();
 
 export class DashboardFactory {
 	static create(): React.ReactElement {
-		return <Dashboard repository={repository} />;
+		return (
+			<Dashboard repository={repository} repositoryWidgetRepository={repositoryWidgetRepository} />
+		);
 	}
 }
