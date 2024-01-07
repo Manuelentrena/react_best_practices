042860397715cb42e932d5d503a57d6ffd918770
EntrenaDev <manuel.entrena@gmail.com>
2024-01-07 15:49:40 +0100

    Added repositories from localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "react-loading-skeleton/dist/skeleton.css";
 
 import { RepositoryWidgetContextProvider } from "./components/providers/RepositoryWidgetContextProvider";
+import { LocalStorageRepositoryWidgetRepository } from "./infrastructure/LocalStorageWidgetRepository";
 import { Router } from "./router/index";
 
+const repository = new LocalStorageRepositoryWidgetRepository();
 export function App() {
 	return (
-		<RepositoryWidgetContextProvider>
+		<RepositoryWidgetContextProvider repository={repository}>
 			<Router />
 		</RepositoryWidgetContextProvider>
 	);
