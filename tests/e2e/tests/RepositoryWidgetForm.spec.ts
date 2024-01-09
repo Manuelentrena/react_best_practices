64abb4c7514a44967ba379b3c16ace11fef334c1
EntrenaDev <manuel.entrena@gmail.com>
2024-01-09 20:18:06 +0100

    Added test e2e, "Add new repository with id and url"

---
+++ tests/e2e/tests/RepositoryWidgetForm.spec.ts
@@ -0,0 +1,31 @@
+import { RepositoryWidgetMother } from "../../RepositoryWidgetMother";
+
+describe("Repository Widget Form", () => {
+	it("Add new repository with id and url", () => {
+		const newWidget = RepositoryWidgetMother.create({
+			repositoryUrl: "https://github.com/CodelyTV/DevDash",
+			id: "123456",
+		});
+
+		cy.visit("/");
+
+		cy.wait(1000).then(() => {
+			cy.findByRole("button", {
+				name: new RegExp("Añadir repositorio", "i"),
+			}).click();
+
+			cy.findByLabelText(/Id/i).type(newWidget.id);
+			cy.findByLabelText(/Url del repositorio/i).type(newWidget.repositoryUrl);
+
+			cy.findByRole("button", {
+				name: /Añadir/i,
+			}).click();
+
+			cy.wait(1000).then(() => {
+				cy.findByText("CodelyTV/react-devdash").then((widget) => {
+					cy.wrap(widget).should("exist");
+				});
+			});
+		});
+	});
+});
