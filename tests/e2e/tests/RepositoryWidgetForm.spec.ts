58fc90ef99164545e9db7713430facc59ffddb1b
EntrenaDev <manuel.entrena@gmail.com>
2024-01-09 20:24:53 +0100

    Added test e2e, "Show error when respository already exists in Dashboard"

--- tests/e2e/tests/RepositoryWidgetForm.spec.ts
+++ tests/e2e/tests/RepositoryWidgetForm.spec.ts
@@ -28,4 +28,46 @@ describe("Repository Widget Form", () => {
 			});
 		});
 	});
+	it("Show error when respository already exists in Dashboard", () => {
+		const newWidget = RepositoryWidgetMother.create({
+			repositoryUrl: "https://github.com/CodelyTV/DevDash",
+			id: "123456",
+		});
+
+		cy.visit("/");
+
+		cy.wait(1000).then(() => {
+			cy.findByRole("button", {
+				name: new RegExp("Añadir repositorio", "i"),
+			}).click();
+
+			cy.findByLabelText(/Id/i).type(newWidget.id);
+			cy.findByLabelText(/Url del repositorio/i).type(newWidget.repositoryUrl);
+
+			cy.findByRole("button", {
+				name: /Añadir/i,
+			}).click();
+
+			cy.wait(1000).then(() => {
+				cy.findByText("CodelyTV/react-devdash").then((widget) => {
+					cy.wrap(widget).should("exist");
+				});
+			});
+
+			cy.findByRole("button", {
+				name: new RegExp("Añadir repositorio", "i"),
+			}).click();
+
+			cy.findByLabelText(/Id/i).type(newWidget.id);
+			cy.findByLabelText(/Url del repositorio/i).type(newWidget.repositoryUrl);
+
+			cy.findByRole("button", {
+				name: /Añadir/i,
+			}).click();
+
+			cy.findByText("URL duplicada").then((widget) => {
+				cy.wrap(widget).should("exist");
+			});
+		});
+	});
 });
