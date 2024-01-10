3ddb599efa738292e0a0e4d4ea51245ccc79ff43
EntrenaDev <manuel.entrena@gmail.com>
2024-01-10 20:35:52 +0100

    Added config in layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -13,11 +13,17 @@ export function Layout() {
 			<TopbarProgresProvider />
 			<header className={styles.header}>
 				<section className={styles.header__container}>
-					<a href="https://codely.com">
-						<Brand />
-					</a>
-					<Link to={`/`}>
-						<h1 className={styles.app__brand}>{title}</h1>
+					<div className={styles.brand__container}>
+						<a href="https://codely.com">
+							<Brand />
+						</a>
+						<Link to="/">
+							<h1 className={styles.app__brand}>{title}</h1>
+						</Link>
+					</div>
+
+					<Link to="/config">
+						<span>⚙️</span>
 					</Link>
 				</section>
 			</header>
